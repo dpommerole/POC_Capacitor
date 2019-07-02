ca4ccd6935e1a6b0f9ab3c8cfe7bfa5a6531b9da
Dimitri Pommerole <dpommerole@atecna.fr>
2019-07-02 17:06:10 +0200

    chore: add first part of push configuration

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # POC_Capacitor
 
 This repo is a POC for Capacitor, based on a PWA built with Vue js.
+
+Source: https://www.smashingmagazine.com/2018/07/mobile-apps-capacitor-vue-js/
--- poc-capacitor/src/views/Home.vue
+++ poc-capacitor/src/views/Home.vue
@@ -1,18 +1,51 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import {
+    Plugins
+  } from '@capacitor/core'
+
+const { PushNotifications } = Plugins
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+  },
+  created () {
+    PushNotifications.register()
+
+    // On succcess, we should be able to receive notifications
+    PushNotifications.addListener('registration',
+      (token) => {
+        alert('Push registration success, token: ' + token.value);
+      }
+    );
+
+    // Some issue with our setup and push will not work
+    PushNotifications.addListener('registrationError',
+      (error) => {
+        alert('Error on registration: ' + JSON.stringify(error));
+      }
+    );
+
+    // Show us the notification payload if the app is open on our device
+    PushNotifications.addListener('pushNotificationReceived',
+      (notification) => {
+        alert('Push received: ' + JSON.stringify(notification));
+      }
+    );
+
+    // Method called when tapping on a notification
+    PushNotifications.addListener('pushNotificationActionPerformed',
+      (notification) => {
+        alert('Push action performed: ' + JSON.stringify(notification));
+      }
+    );
   }
 }
 </script>
